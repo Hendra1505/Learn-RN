b9e25e265a2de06f0339a9f32107d9dde24dc1c4
hendraes <hendraekky1946@gmail.com>
2023-10-01 21:18:03 +0700

    Learn Chapter 8, Creating functional component

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+
+const App = () => {
+
+  return (
+    <Text> IniKarir </Text>
+  );
+
+};
+
+export default App;
