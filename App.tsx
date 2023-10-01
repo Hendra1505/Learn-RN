2c958ab943cfd6effaacc57a5e810d0a1e09f33b
hendraes <hendraekky1946@gmail.com>
2023-10-01 21:36:43 +0700

    Add all new component into RN Apps

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+import Texts from './src/components/Texts';
+import Photos from './src/components/Photos';
 
 const App = () => {
 
   return (
-    <Text> IniKarir </Text>
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}} />
+      <Text>Hendra</Text>
+      <Texts />
+      <Photos />
+    </View>
   );
 
 };
