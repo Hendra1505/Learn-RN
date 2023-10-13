4a96ff486ca3d2716b4781266ab6198b79027611
hendraes <hendraekky1946@gmail.com>
2023-10-13 09:08:56 +0700

    [Hendra] Poppins font

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
+import { useRouter } from 'expo-router';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hai Ekky!</Text>
+        <Text style={styles.welcomeMessage}>Cari pekerjaan impianmu</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() => {}}
+            placeholder='Cari pekerjaan'/>
+        </View>
+      </View>
     </View>
   )
 }
