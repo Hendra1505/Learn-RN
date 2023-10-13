bdfa16997e4d36b26ec154ca12bd9e3ef91e7cc0
hendraes <hendraekky1946@gmail.com>
2023-10-13 09:07:36 +0700

    [Hendra] adding fonts and SplashScreen condition

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,28 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />;
+    const [fontsLoaded] = useFonts({
+        PBold: require('../assets/fonts/Poppins-Bold.ttf'),
+        PMedium: require('../assets/fonts/Poppins-Medium.ttf'),
+        PRegular: require('../assets/fonts/Poppins-Regular.ttf')
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        // Membuat sebuah kondisi apabila fonts belum di loaded maka SplashScreen tetap berjalan hingga
+        // Menunggu fonts telah usai dan baru SplashScreen ke hide
+        if(fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null;
+
+    return <Stack onLayout={ onLayoutRootView } />;
 }
 
 export default Layout;
