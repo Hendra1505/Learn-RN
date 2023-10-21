a1beb587382e3c8f2fb368ca3336f4214f1f3be4
hendraes <hendraekky1946@gmail.com>
2023-10-21 15:28:14 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>
+          Another piece of Text guys
+        </Text>
+      </View>
+      <Text 
+        style={{
+          margin: 16,
+          borderWidth: 2,
+          borderColor: 'green',
+        }}>
+        This was a Text
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
